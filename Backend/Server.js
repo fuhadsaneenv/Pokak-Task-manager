0ea77c0ae06f1997b4418d52c425108a96d429e2
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-08-14 12:09:42 +0530

    vercel and render link updated

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,35 +2,73 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/Config/dbConfig.js";
 import UserRouter from "./src/Routes/userRoute.js";
-import errorHandler from "./src/Middleware/errorHandler.js";
-import cors from "cors"
 import Taskrouter from "./src/Routes/taskRoute.js";
+import errorHandler from "./src/Middleware/errorHandler.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-    })
+  cors({
+    origin: "https://pokak-task-manager-x3sq-g60dfua1p.vercel.app/",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
+
 connectDB();
 
-app.use('/api/user', UserRouter);
-app.use('/api/tasks', Taskrouter);
+// Backend routes
+app.use("/api/user", UserRouter);
+app.use("/api/tasks", Taskrouter);
 
-
-
-app.use(errorHandler)
+// Error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import connectDB from "./src/Config/dbConfig.js";
+// import UserRouter from "./src/Routes/userRoute.js";
+// import errorHandler from "./src/Middleware/errorHandler.js";
+// import cors from "cors"
+// import Taskrouter from "./src/Routes/taskRoute.js";
+// import cookieParser from "cookie-parser";
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(
+//     cors({
+//         origin: ['http://localhost:5173',
+//         "https://pokak-task-manager-x3sq-g60dfua1p.vercel.app"],
+
+//         credentials: true,
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type', 'Authorization'],
+//     })
+// );
+// connectDB();
+
+// app.use('/api/user', UserRouter);
+// app.use('/api/tasks', Taskrouter);
+
+// app.use(errorHandler)
+
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on Port ${PORT}`);
+// });
