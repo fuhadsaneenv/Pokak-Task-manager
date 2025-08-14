d5fb839dff67123ea8d1a0a3d970f677dbf221d0
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-08-14 12:41:08 +0530

    deploy

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,14 +13,27 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "https://pokak-task-manager-x3sq.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const allowedOrigins = [
+    "https://pokak-task-manager-x3sq.vercel.app"
+  ];
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
+// app.use(
+//   cors({
+// //     origin: "https://pokak-task-manager-x3sq.vercel.app",
+// //     credentials: true,
+// //     methods: ["GET", "POST", "PUT", "DELETE"],
+// //     allowedHeaders: ["Content-Type", "Authorization"],
+// //   })
+// // );
 
 connectDB();
 
