0affa1659c39bc71ca7f5ac5db58a3d004248fa7
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-08-14 11:05:11 +0530

    Deployed error handling

--- Frontend/src/Layout/Dashboardlayout.jsx
+++ Frontend/src/Layout/Dashboardlayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axiosInstance from '../axiosInstance';
 import { DashNavbar } from '../Components/DashUI/DashNavbar';
 import { DashSidebar } from '../Components/DashUI/DashSidebar';
-import { Taskview } from '../Components/DashUI/Taskview';
+import { Taskview } from '../Components/DashUI/Tasksview';
 import { TaskCreatePage } from '../Components/DashUI/TaskCreatePage';
 
  export function DashLayout() {
