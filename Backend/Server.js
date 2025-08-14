415f1ec8696f5e21c060e93861ff08c3f139caf7
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-08-14 12:14:45 +0530

    vercel and render link updated

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://pokak-task-manager-x3sq-g60dfua1p.vercel.app/",
+    origin: "https://pokak-task-manager-x3sq.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
