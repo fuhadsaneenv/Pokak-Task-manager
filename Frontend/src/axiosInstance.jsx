060fabb569f346fee2044430767f29e340957753
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-08-14 12:31:14 +0530

    deploy

--- Frontend/src/axiosInstance.jsx
+++ Frontend/src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ,
+  baseURL: `https://pokak-task-manager.onrender.com` ,
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
